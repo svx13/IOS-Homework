d6a2740cb118238ef173a745a53cda37f8263bed
svx13 <svxsvx@icloud.com>
2022-05-22 14:06:03 +0500

    Добавлено д.з.2.3 часть 1

--- Navigation/TabBarController.swift
+++ Navigation/TabBarController.swift
@@ -5,15 +5,30 @@
 //  Created by Саша on 05.05.2022.
 //
 
+
+extension UIColor {
+    convenience init(hex: Int) {
+        let components = (
+            R: CGFloat((hex >> 16) & 0xff) / 255,
+            G: CGFloat((hex >> 08) & 0xff) / 255,
+            B: CGFloat((hex >> 00) & 0xff) / 255
+        )
+        self.init(red: components.R, green: components.G, blue: components.B, alpha: 1)
+    }
+}
+
 import UIKit
 
 class TabBarController: UITabBarController {
     
     let feedViewController = FeedViewController()
-    let profileViewController = ProfileViewController()
+    let logInViewController = LogInViewController()
+    let colorSet = UIColor(hex:0x4885CC)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        self.view.tintColor = UIColor(ciColor: .init(color: colorSet))
         setupControllers()
     }
     
@@ -21,14 +36,16 @@ class TabBarController: UITabBarController {
         
         let navController = UINavigationController(rootViewController: feedViewController)
         feedViewController.tabBarItem.title = "Лента пользователя"
-        feedViewController.tabBarItem.image = UIImage(systemName: "bookmark")
+        feedViewController.tabBarItem.image = UIImage(systemName: "house.fill")
         feedViewController.navigationItem.title = "Лента пользователя"
         
-        let navController2 = UINavigationController(rootViewController: profileViewController)
-        profileViewController.tabBarItem.title = "Профиль пользователя"
-        profileViewController.tabBarItem.image = UIImage(systemName: "mail")
-        profileViewController.navigationItem.title = "Профиль пользователя"
+        let navController2 = UINavigationController(rootViewController: logInViewController)
+        logInViewController.tabBarItem.title = "Профиль пользователя"
+        logInViewController.tabBarItem.image = UIImage(systemName: "person.fill")
+        logInViewController.navigationItem.title = "Профиль пользователя"
         
         viewControllers = [navController, navController2]
     }
 }
+
+
